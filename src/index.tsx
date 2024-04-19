126d2133f43e3df968d2ae9eea4fd077cc92c7fb
CharlesKristov <charlesdarmawan1@gmail.com>
2024-04-19 17:33:24 +0700

    add hello!

--- src/index.tsx
+++ src/index.tsx
@@ -8,23 +8,33 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from 'react-redux';
 import { store } from './config/store/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <GoogleOAuthProvider clientId="35147913259-9n99c9q7at8ose24drh1d5tu9cecjh1q.apps.googleusercontent.com">
-        <App />
-      </GoogleOAuthProvider>
-      {/* </PersistGate> */}
-    </Provider>
-  </React.StrictMode>
-);
+const index = () => {
+  return(
+    <>
+      <h1>hello!</h1>
+    </>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       {/* <PersistGate loading={null} persistor={persistor}> */}
+//       <GoogleOAuthProvider clientId="35147913259-9n99c9q7at8ose24drh1d5tu9cecjh1q.apps.googleusercontent.com">
+//         <App />
+//       </GoogleOAuthProvider>
+//       {/* </PersistGate> */}
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
